6d0553f191f62594d226bb6a6efd08132b942a4c
Jens Smith <qedpro@hotmail.com>
2017-04-17 20:09:54 -0700

    database updates

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -3,9 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Book;
 
 class PracticeController extends Controller
 {
+
+    public function practice8() {
+
+    }
+
+    public function practice7() {
+        $result = Book::orderBy('title', 'desc')->get();
+        dump($result->toArray());
+    }
+
+    public function practice6() {
+        $result = Book::orderBy('created_at')->limit(3)->get();
+        dump($result->toArray());
+    }
+
+    public function practice5() {
+        $result = Book::orderBy('published', 'desc')->get();
+        dump($result->toArray());
+    }
+
+    public function practice4() {
+        $results = Book::where('author', '=', 'F. Scott Fitzgerald')->get();
+        dump($results->toArray()); # Study the results
+    }
+
+    public function practice3() {
+        $results = Book::where('published', '>', 1950)->get();
+        dump($results->toArray()); # Study the results
+    }
     /**
     *
     */
